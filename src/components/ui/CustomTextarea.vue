e117d9a191c5ca398d5efd7ed9386eff300970dc
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2025-02-20 17:21:48 +0300

    Доработан CustomTextarea

--- src/components/ui/CustomTextarea.vue
+++ src/components/ui/CustomTextarea.vue
@@ -4,70 +4,73 @@ import { nextTick, ref, watch } from 'vue'
 const { value } = defineProps({
   value: {
     type: String,
+    required: true,
   },
   placeholder: {
     type: String,
   },
 })
 
-const emit = defineEmits(['update:value', 'send'])
+const emit = defineEmits(['update:value', 'keydown-send'])
 
-const textarea = ref(null)
+const textareaRef = ref(null)
 
 watch(
   () => value,
-  () => {
-    resize()
-  },
+  () => resize(),
 )
-
-const onChangeHandler = (e) => {
-  emit('update:value', e.target.value)
-}
 
 const resize = () => {
   nextTick(() => {
-    const el = textarea.value
-    if (el) {
-      el.style.height = 'auto' // Сброс высоты
-      el.style.height = el.scrollHeight + 'px'
-    }
+    const element = textareaRef.value
+
+    element.style.height = 'auto'
+    element.style.height = element.scrollHeight + 'px'
   })
 }
 
-const handleKeyDown = (event) => {
+const inputHandler = (event) => {
+  emit('update:value', event.target.value)
+}
+
+const keydownHandler = (event) => {
   if (event.key === 'Enter') {
     if (event.shiftKey) {
       return
     }
     event.preventDefault()
-    emit('send')
+    emit('keydown-send')
   }
 }
+
+const focusTextarea = () => {
+  textareaRef.value.focus()
+}
+
+defineExpose({ focusTextarea })
 </script>
 
 <template>
   <textarea
-    ref="textarea"
+    ref="textareaRef"
     :value
-    @input="onChangeHandler"
     :placeholder
     rows="1"
-    @keydown="handleKeyDown"
+    @input="inputHandler"
+    @keydown="keydownHandler"
   />
 </template>
 
 <style scoped>
 textarea {
-  color: var(--color-text);
+  max-height: 230px;
+  flex-grow: 1;
+  border: none;
+  border-radius: 5px;
+  outline: none;
   resize: none;
   background: none;
-  border: none;
-  flex: 1;
-  padding: 5px;
-  border-radius: 5px;
   font: inherit;
-  outline: none;
-  max-height: 230px;
+  color: var(--color-text);
 }
 </style>
