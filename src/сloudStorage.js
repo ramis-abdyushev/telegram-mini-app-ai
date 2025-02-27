a7193bbb8ec06a2f1dcbd46e9d20b4cd3ad5a1a3
Ramis Abdyushev <ramis_abdyushev@mail.ru>
2025-02-28 01:03:43 +0300

    Тестирование максимальной длины записи телеграм хранилища

--- src/сloudStorage.js
+++ src/сloudStorage.js
@@ -8,66 +8,66 @@ function getKeys() {
   return new Promise((resolve, reject) => {
     cloudStorage.getKeys((err, value) => {
       if (err) {
-        reject(err); // Если ошибка, отклоняем промис
+        reject(err) // Если ошибка, отклоняем промис
       } else {
-        resolve(value); // Если всё ок, разрешаем промис с ключами
+        resolve(value) // Если всё ок, разрешаем промис с ключами
       }
-    });
-  });
+    })
+  })
 }
 
 function removeItems(keys) {
   return new Promise((resolve, reject) => {
     cloudStorage.removeItems(keys, (err, value) => {
       if (err) {
-        reject(err); // Если ошибка, отклоняем промис
+        reject(err) // Если ошибка, отклоняем промис
       } else {
-        resolve(value); // Если всё ок, разрешаем промис с ключами
+        resolve(value) // Если всё ок, разрешаем промис с ключами
       }
-    });
-  });
+    })
+  })
 }
 
 function getItem(key) {
   return new Promise((resolve, reject) => {
     cloudStorage.getItem(key, (err, value) => {
       if (err) {
-        reject(err); // Если ошибка, отклоняем промис
+        reject(err) // Если ошибка, отклоняем промис
       } else {
-        resolve(value); // Если всё ок, разрешаем промис с ключами
+        resolve(value) // Если всё ок, разрешаем промис с ключами
       }
-    });
-  });
+    })
+  })
 }
 
 function getItems(keys) {
   return new Promise((resolve, reject) => {
     cloudStorage.getItems(keys, (err, value) => {
       if (err) {
-        reject(err); // Если ошибка, отклоняем промис
+        reject(err) // Если ошибка, отклоняем промис
       } else {
-        resolve(value); // Если всё ок, разрешаем промис с ключами
+        resolve(value) // Если всё ок, разрешаем промис с ключами
       }
-    });
-  });
+    })
+  })
 }
 
 function setItem(key, value) {
   return new Promise((resolve, reject) => {
     cloudStorage.setItem(key, value, (err, value) => {
       if (err) {
-        reject(err); // Если ошибка, отклоняем промис
+        reject(err) // Если ошибка, отклоняем промис
       } else {
-        resolve(value); // Если всё ок, разрешаем промис с ключами
+        resolve(value) // Если всё ок, разрешаем промис с ключами
       }
-    });
-  });
+    })
+  })
 }
 
 export async function cleanChats() {
   const messagesStore = useMessagesStore()
   messagesStore.setMessages([])
-  const allKeys = await getKeys();
+  const allKeys = await getKeys()
   await removeItems(allKeys)
 }
 
@@ -94,7 +94,7 @@ export async function addMessageToChat(message) {
   const addedChunks = splitString(messageStr, availableLength)
 
   for (let index = 0; index < addedChunks.length; index++) {
-    let value = addedChunks[index];
+    let value = addedChunks[index]
 
     if (index === 0) {
       value = lastChunkStr + value
@@ -114,7 +114,7 @@ export async function addMessageToChat(message) {
     chatCount++
   }
 
-  const allKeysA = await getKeys();
+  const allKeysA = await getKeys()
 
   console.log('R1', allKeysA)
   console.log('R2', await getItems(allKeysA))
@@ -145,7 +145,20 @@ function splitString(string, firstChunkLength) {
 }
 
 export async function loadChat() {
-  const countStr = await getItem('chat_count');
+  let messs =
+    '{"role":"user","content":"Дай большой текст"},{"role":"assistant","content":"Напишите мне, о чем вы хотите написать. Я могу предоставить вам информацию или assisting с созданием текста на любой тему, которая вам интересна."},{"role":"user","content":"Дай большой текст"},{"role":"assistant","content":"Если вы ищете информацию на конкретную тему, я могу предоставить вам подробный текст.\\n\\nЕсли вы хотите ознакомиться с большим текстом по разным темам, я могу предложить следующие варианты:\\n\\n1. Историческая справка: Я могу предоставить вам detailed и историческую информацию о различных событиях, периодах или cultures.\\n2. ФilosOFическая концепция: Я могу объяснить вам сложные философские идеи и концепции на разные темы, такие как этика, логика, психология и более.\\n3. Технологические информации: Я могу предоставить вам подробную информацию о различных технологиях, such как компьютерная техника, мобильная technology, интернет и более.\\n4. Суровые реалности: Я могу рассказать вам об исторических событиях, таких как политические потрясения, военные конфликты, экономические кризисы и более.\\n\\nКоторый раздел вы предпочтет?"},{"role":"user","content":"Дай большой текст"},{"role":"assistant","content":"Я понз наиболее значительных империй в истории: Римская империя. В Риме была создана система государственного управления, которая привела к великим достижениям в области инженерии, architecture и art.\\n*   **Греция**. К where many of the most important philosophical and scientific ideas were developed.\\n\\n**Восток и Далекий Восток**\\n\\nВ средние веки (в 8 веке до н. э.) выросли две великие цивилизации: Китай и Индия. В Китае появилась система государственного управления, которая привела к великим достижениям в области инженерии, architecture и technology.\\n\\n*   **Китай**. В Китае была создана система госукий Восток**\\n\\nВ средние веки (в 8 веке до н. э.) выросли две великие цивилизации: Китай и Индия. В Китае появилась система государственного управления, которая привела к великим достижениям в области инженерии, architecture и technology.\\n\\n*   **Китай**. В Китае была создана система госу'
+
+  try {
+    while (true) {
+      console.log(messs.length)
+      await setItem(`qwe`, messs)
+      messs += 'a'
+    }
+  } catch (e) {
+    console.error('Длина', e, messs.length)
+  }
+
+  const countStr = await getItem('chat_count')
 
   console.log('RRR', countStr)
 
@@ -153,13 +166,13 @@ export async function loadChat() {
     return
   }
 
-  let count = Number(countStr);
-  let fullChat = '';
+  let count = Number(countStr)
+  let fullChat = ''
 
   for (let i = 1; i <= count; i++) {
-    let chunk = await getItem(`chat_${i}`);
+    let chunk = await getItem(`chat_${i}`)
     if (chunk) {
-      fullChat += chunk;
+      fullChat += chunk
     }
   }
 
